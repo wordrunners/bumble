8dd74ffecdfe6e9d2e6b10d97161a6f3751de401
Danil <ev8ko@icloud.com>
2022-11-24 02:12:18 +0400

    fix test

--- packages/client/src/tests/App.test.tsx
+++ packages/client/src/tests/App.test.tsx
@@ -1,5 +1,6 @@
 import App from './App'
 import { render, screen } from '@testing-library/react'
+import { expect, test } from '@jest/globals';
 
 const appContent = 'Вот тут будет жить ваше приложение :)'
 
