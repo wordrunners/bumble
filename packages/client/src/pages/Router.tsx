4bb5461ddeba66a9cc9e4db9135f1a373b026b5b
Andrey-A-A <stuff-box@bk.ru>
2022-11-27 17:28:33 +0300

    Task 20 passwordApi

--- packages/client/src/pages/Router.tsx
+++ packages/client/src/pages/Router.tsx
@@ -7,6 +7,8 @@ import { GamePlayPage } from '@/pages'
 import { GameOverPage } from '@/pages'
 import { SigninPage } from './signinPage'
 import { SignupPage } from './signupPage'
+import { ProfilePage } from './profilePage'
+import { ChangePasswordPage } from '@/pages/changePasswordPage'
 
 export const Router = createBrowserRouter([
   {
@@ -42,6 +44,44 @@ export const Router = createBrowserRouter([
   {
     path: '/signup',
     element: <SignupPage />,
+    errorElement: <ErrorPage />,
+    children: [],
+  },
+  {
+    path: '/profile',
+    element: (
+      <ProfilePage
+        id={31}
+        login={'Семен'}
+        firstName={'Семен'}
+        secondName={'Семенов'}
+        displayName={'Семен'}
+        avatar={
+          '/46f3061f-ca1f-4a29-8f0e-a921109bdc10/24b0e54b-9e9f-4e8e-94d7-dfd129a4d58a_round-avatar.png'
+        }
+        phone={'+789545623'}
+        email={'aasd@bk.ru'}
+      />
+    ),
+    errorElement: <ErrorPage />,
+    children: [],
+  },
+  {
+    path: '/change-password',
+    element: (
+      <ChangePasswordPage
+        id={31}
+        login={'Семен'}
+        firstName={'Семен'}
+        secondName={'Семенов'}
+        displayName={'Семен'}
+        avatar={
+          '/46f3061f-ca1f-4a29-8f0e-a921109bdc10/24b0e54b-9e9f-4e8e-94d7-dfd129a4d58a_round-avatar.png'
+        }
+        phone={'+789545623'}
+        email={'aasd@bk.ru'}
+      />
+    ),
     errorElement: <ErrorPage />,
     children: [],
   },
