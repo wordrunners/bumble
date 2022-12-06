3f3ad65ba8266643f0ee777987db27cc6b11a482
Andrey-A-A <76514999+Andrey-A-A@users.noreply.github.com>
2022-12-07 00:10:12 +0300

    Task 18 profile (#9)

--- packages/client/src/pages/Router.tsx
+++ packages/client/src/pages/Router.tsx
@@ -49,39 +49,13 @@ export const Router = createBrowserRouter([
   },
   {
     path: '/profile',
-    element: (
-      <ProfilePage
-        id={31}
-        login={'Семен'}
-        firstName={'Семен'}
-        secondName={'Семенов'}
-        displayName={'Семен'}
-        avatar={
-          '/46f3061f-ca1f-4a29-8f0e-a921109bdc10/24b0e54b-9e9f-4e8e-94d7-dfd129a4d58a_round-avatar.png'
-        }
-        phone={'+789545623'}
-        email={'aasd@bk.ru'}
-      />
-    ),
+    element: <ProfilePage />,
     errorElement: <ErrorPage />,
     children: [],
   },
   {
     path: '/change-password',
-    element: (
-      <ChangePasswordPage
-        id={31}
-        login={'Семен'}
-        firstName={'Семен'}
-        secondName={'Семенов'}
-        displayName={'Семен'}
-        avatar={
-          '/46f3061f-ca1f-4a29-8f0e-a921109bdc10/24b0e54b-9e9f-4e8e-94d7-dfd129a4d58a_round-avatar.png'
-        }
-        phone={'+789545623'}
-        email={'aasd@bk.ru'}
-      />
-    ),
+    element: <ChangePasswordPage />,
     errorElement: <ErrorPage />,
     children: [],
   },
--- packages/client/src/pages/profilePage/profilePage.tsx
+++ packages/client/src/pages/profilePage/profilePage.tsx
@@ -1,20 +1,47 @@
-import React, { FC } from 'react'
+import React, { FC, useContext } from 'react'
 import { Link } from 'react-router-dom'
-import type { User } from '../../components/Avatar'
-import { Avatar } from '../../components/Avatar'
+import type { User } from '@/components/Avatar'
+import { Avatar } from '@/components/Avatar'
+import './profilePage.scss'
+import '@/assets/images/avatar.png'
+import { AppContext } from '@/Core'
 
-export const ProfilePage: FC<User> = (props: User) => {
-  const submitForm = (event: { preventDefault: () => void }) => {
-    event.preventDefault()
-  }
-
+export const ProfilePage: FC<User> = () => {
+  const user = useContext(AppContext)
   return (
-    <>
-      <Avatar displayName={'Семен'} avatar={props.avatar} />
-      <form
-        className="profile__form"
-        onSubmit={event => submitForm(event)}></form>
-      <Link to={`/change-password`}>{<p>Изменение пароля</p>}</Link>
-    </>
+    <div className="wrap">
+      <Avatar displayName={user.displayName} avatar={user.avatar} />
+      <div className="data__list">
+        <span>Почта</span>
+        <span>{user.email}</span>
+      </div>
+      <div className="data__list">
+        <span>Логин</span>
+        <span>{user.login}</span>
+      </div>
+      <div className="data__list">
+        <span>Имя</span>
+        <span>{user.firstName}</span>
+      </div>
+      <div className="data__list">
+        <span>Фамилия</span>
+        <span>{user.secondName}</span>
+      </div>
+      <div className="data__list">
+        <span>Имя в игре</span>
+        <span>{user.displayName}</span>
+      </div>
+      <div className="data__list">
+        <span>Телефон</span>
+        <span>{user.phone}</span>
+      </div>
+      <div className="management">
+        <Link className="link" to={`/change-settings`}>
+          {<p>Изменить данные</p>}
+        </Link>
+        <Link to={`/change-password`}>{<p>Измененить пароль</p>}</Link>
+        <Link to={`/`}>{<p>Назад</p>}</Link>
+      </div>
+    </div>
   )
 }
