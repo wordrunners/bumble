5f8052be44b416632e030d7dd5178cb7f2fc785d
Danil <ev8ko@icloud.com>
2022-12-16 19:02:58 +0400

    fix tabs to spaces

--- packages/client/src/pages/leaderboardPage/leaderboardPage.tsx
+++ packages/client/src/pages/leaderboardPage/leaderboardPage.tsx
@@ -7,20 +7,20 @@ import { selectLeaders } from '@/store/leaderBoardSlice'
 import { Leader } from '@/types'
 
 export const LeaderboardPage = (): JSX.Element => {
-	const leaders = useAppSelector(selectLeaders)
+  const leaders = useAppSelector(selectLeaders)
 
-	return (
-		<section className='leaderboard'>
-			<div className='leaderboard__header'>
-				<img src={Rule} className='leaderboard__icon' />
-				<h2 className='leaderboard__title'>ЛИДЕРЫ</h2>
-				<img src={Bag} className='leaderboard__icon leaderboard__icon_rotated' />
-			</div>
-			<div className='leaderboard__list'>
-				{leaders.map((leader: Leader, i) => (
-					<LeaderboardRow key={i} leader={leader} />
-				))}
-			</div>
-		</section>
-	);
+  return (
+    <section className='leaderboard'>
+      <div className='leaderboard__header'>
+        <img src={Rule} className='leaderboard__icon' />
+        <h2 className='leaderboard__title'>ЛИДЕРЫ</h2>
+        <img src={Bag} className='leaderboard__icon leaderboard__icon_rotated' />
+      </div>
+      <div className='leaderboard__list'>
+        {leaders.map((leader: Leader, i) => (
+          <LeaderboardRow key={i} leader={leader} />
+        ))}
+      </div>
+    </section>
+  );
 };
--- packages/client/src/pages/startPage/startPage.tsx
+++ packages/client/src/pages/startPage/startPage.tsx
@@ -5,36 +5,36 @@ import { LinkButton } from '@/components/LinkButton';
 import { Header } from '@/components/Header';
 import { useEffect } from 'react'
 import {
-	setLeaders,
-	selectLeaders,
+  setLeaders,
+  selectLeaders,
 } from '@/store/leaderBoardSlice'
 import { 
-	useAppSelector, 
-	useAppDispatch 
+  useAppSelector, 
+  useAppDispatch 
 } from '@/hooks'
 import { Leaders } from '@/types'
 import leadersData from '@/data/leaders.json'
 
 export const StartPage = (): JSX.Element => {
-	const dispatch = useAppDispatch()
-	const leaders = useAppSelector(selectLeaders)
+  const dispatch = useAppDispatch()
+  const leaders = useAppSelector(selectLeaders)
 
-	useEffect(() => {
-		if (leaders.length === 0) {
-			const newLeaders: Leaders = []
-			leadersData.map((leader, i) => {
-				newLeaders.push(leader)
-			})
-			dispatch(setLeaders(newLeaders))
-		}
-	}, [])
+  useEffect(() => {
+    if (leaders.length === 0) {
+      const newLeaders: Leaders = []
+      leadersData.map((leader, i) => {
+        newLeaders.push(leader)
+      })
+      dispatch(setLeaders(newLeaders))
+    }
+  }, [])
 
-	return (
-		<section className={cn('start')}>
-			<Header />
-			<img src={Logo} alt='logo' className={cn('start__logo')} />
-			<LinkButton to='/game' modifier='game-btn'>НАЧАТЬ ИГРУ</LinkButton>
-			<LinkButton to='/signin' modifier='login-btn'>ВОЙТИ</LinkButton>
-		</section> 
-	)
+  return (
+    <section className={cn('start')}>
+      <Header />
+      <img src={Logo} alt='logo' className={cn('start__logo')} />
+      <LinkButton to='/game' modifier='game-btn'>НАЧАТЬ ИГРУ</LinkButton>
+      <LinkButton to='/signin' modifier='login-btn'>ВОЙТИ</LinkButton>
+    </section> 
+  )
 };
