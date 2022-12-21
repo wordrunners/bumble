a04a9820ac21f6837113368a86c8b3fc8eb97f45
pritornyi <marchenko.ar@icloud.com>
2022-12-22 02:02:30 +0300

    Add Button.test.tsx, add @testing-library/jest-dom

--- packages/client/src/tests/Button.test.tsx
+++ packages/client/src/tests/Button.test.tsx
@@ -1,9 +1,34 @@
 import { Button } from '../components/Button'
-import { render } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 
-test('Button', async () => {
-  const button = render(
-    <Button onClick={() => ''}>This is Button!</Button>
-  );
-  expect(button).toMatchSnapshot();
-}); 
+const testingContentString = 'Test'
+it('Component should be rendered', async () => {
+  render(<Button children={testingContentString}/>)
+  expect(screen.getByText(testingContentString)).toBeDefined()
+})
+
+it('Element should return button', () => {
+  render(<Button />);
+  expect(screen.getByRole('button')).toBeTruthy();
+});
+
+describe('Should properties change state', ()=> {
+  it('Change children prop', ()=>{
+      const component = render(<Button children={testingContentString}/>)
+      expect(screen.findByText(testingContentString)).toBeTruthy()
+      expect(component).toMatchSnapshot();
+
+      const anyOtherContent = 'Any'
+      component.rerender(<Button children={anyOtherContent}/>)
+      expect(screen.getByText(anyOtherContent)).toBeTruthy()
+      expect(component).toMatchSnapshot();
+    }
+  )
+})
+
+test('Should call onClick prop when clicked', () => {
+  const handleClick = jest.fn()
+  render(<Button onClick={handleClick}>Click Me</Button>)
+  fireEvent.click(screen.getByRole('button'))
+  expect(handleClick).toHaveBeenCalledTimes(1)
+})
