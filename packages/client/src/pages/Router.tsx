c6610e91ba42e32e8086e89fd3291e54367fdcc6
Danil <ev8ko@icloud.com>
2023-01-13 16:57:32 +0400

    fix

--- packages/client/src/pages/Router.tsx
+++ packages/client/src/pages/Router.tsx
@@ -10,21 +10,34 @@ import { ForumPage } from './forumPage'
 import { ChangePasswordPage } from '@/pages/changePasswordPage'
 import { LeaderboardPage } from './leaderboardPage'
 import { Route, Routes } from 'react-router-dom'
+import { 
+  ROOT_ROUTE,
+  GAME_ROUTE,
+  GAME_PLAY_ROUTE,
+  GAME_OVER_ROUTE,
+  SIGNIN_ROUTE,
+  SIGNUP_ROUTE,
+  PROFILE_ROUTE,
+  CHANGE_PASSWORD_ROUTE,
+  LEADERBOARD_ROUTE,
+  FORUM_ROUTE,
+  ERROR_ROUTE,
+} from '@/data/routes'
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<StartPage />} />
-      <Route path="/game" element={<GameStartPage />} />
-      <Route path="/game-play" element={<GamePlayPage />} />
-      <Route path="/game-over" element={<GameOverPage />} />
-      <Route path="/signin" element={<SigninPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/profile" element={<ProfilePage />} />
-      <Route path="/change-password" element={<ChangePasswordPage />} />
-      <Route path="/leaderboard" element={<LeaderboardPage />} />
-      <Route path="/forum" element={<ForumPage />} />
-      <Route path='*' element={<ErrorPage />}/>
+      <Route path={ROOT_ROUTE} element={<StartPage />} />
+      <Route path={GAME_ROUTE} element={<GameStartPage />} />
+      <Route path={GAME_PLAY_ROUTE} element={<GamePlayPage />} />
+      <Route path={GAME_OVER_ROUTE} element={<GameOverPage />} />
+      <Route path={SIGNIN_ROUTE} element={<SigninPage />} />
+      <Route path={SIGNUP_ROUTE} element={<SignupPage />} />
+      <Route path={PROFILE_ROUTE} element={<ProfilePage />} />
+      <Route path={CHANGE_PASSWORD_ROUTE} element={<ChangePasswordPage />} />
+      <Route path={LEADERBOARD_ROUTE} element={<LeaderboardPage />} />
+      <Route path={FORUM_ROUTE} element={<ForumPage />} />
+      <Route path={ERROR_ROUTE} element={<ErrorPage />}/>
     </Routes>
   )
 }
