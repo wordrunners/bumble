33b942a2d27da61c40410bb28df86bfebe35c962
pritornyi <marchenko.ar@icloud.com>
2023-01-14 05:54:09 +0300

    Восстановил работу плеера

--- packages/client/src/components/MusicPlayer/MusicPlayer.tsx
+++ packages/client/src/components/MusicPlayer/MusicPlayer.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import './MusicPlayer.scss';
 import BackgroundMusic from '../../assets/audio/POL-super-match-short.wav'
-import { MuteUnmute } from '@/components/MusicPlayer/controls/MuteUnmute'
+import { Button } from '@/components/Button'
 
 export const MusicPlayer = () => {
   const [isPlaying, setIsPlaying] = useState(false);
@@ -13,41 +13,38 @@ export const MusicPlayer = () => {
     instance.defaultMuted = false;
     instance.autoplay = true;
   }
-  const click = () =>{
-    startupConfig(audioRef.current);
-    audioRef.current.play().catch(e => console.error(e))
+  const toggleIsPlaying = () =>{
+    setIsPlaying(!isPlaying)
     console.log('click', audioRef.current)
   }
+  startupConfig(audioRef.current);
 
   // audioRef.current.addEventListener("canplaythrough", event => {
   //   /* аудио может быть воспроизведено; проиграть, если позволяют разрешения */
-  //   startupConfig(audioRef.current)
-  //   audioRef.current.play().catch((e: Error) => {
-  //     console.error(e.message)
-  //   });
+  //   setIsPlaying(!isPlaying);
   // });
 
-  // useEffect(() => {
-  //   if (isPlaying) {
-  //     audioRef.current.play().catch((e: Error) => {
-  //       console.error(e.message)
-  //     });
-  //   } else {
-  //     audioRef.current.pause();
-  //   }
-  // }, [isPlaying]);
-  //
-  // useEffect(() => {
-  //   // Pause and clean up on unmount
-  //   return () => {
-  //     audioRef.current.pause();
-  //   };
-  // }, []);
+  useEffect(() => {
+    if (isPlaying) {
+      audioRef.current.play().catch((e: Error) => {
+        console.error(e.message)
+      });
+    } else {
+      audioRef.current.pause();
+    }
+  }, [isPlaying]);
+
+  useEffect(() => {
+    // Pause and clean up on unmount
+    return () => {
+      audioRef.current.pause();
+    };
+  }, []);
 
   return (
     <div className="audio-player">
       <div className="audio-controls">
-        <MuteUnmute onClick={()=>click()}></MuteUnmute>
+        <Button className={'toggleMusicButton'} onClick={()=>toggleIsPlaying()}>{!isPlaying ? 'Unmute' : 'mute'}</Button>
       </div>
     </div>
   )
