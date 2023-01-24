e6baea7e8e1b6852bee998ccc78aaa7769f1844f
Danil <ev8ko@icloud.com>
2023-01-24 17:23:18 +0400

    chanched readme + fix oAuth

--- packages/client/src/pages/profilePage/profilePage.tsx
+++ packages/client/src/pages/profilePage/profilePage.tsx
@@ -4,18 +4,13 @@ import { AppDispatch } from '@/store/store'
 import { selectUser } from '@/store/authSlice';
 import { ProfileData } from './profileData'
 import { fetchUser } from '@/store/authSlice';
-import { useNavigate } from 'react-router-dom';
 
 export const ProfilePage: FC = () => {
 
   const userState = useSelector(selectUser);
   const dispatch = useDispatch<AppDispatch>();
-  const navigate = useNavigate();
   useEffect(() => {
     dispatch(fetchUser());
-    if (!userState) {
-      navigate('/')
-    }
   }, []);
 
   return (
