21abf0fb3d84b3e87d165d147ef3644c1d17ff61
Danil <ev8ko@icloud.com>
2023-02-03 17:04:15 +0400

    fix

--- packages/client/src/pages/leaderboardPage/leaderboardPage.tsx
+++ packages/client/src/pages/leaderboardPage/leaderboardPage.tsx
@@ -20,7 +20,6 @@ export const LeaderboardPage = (): JSX.Element => {
 
   useEffect(() => {
     dispatch(fetchLeaderboard());
-    // dispatch(fetchUser());
   }, []);
   
   const currentLeader: Leader = leaders.find(item => item.data.id === user?.id) || {} as Leader;
